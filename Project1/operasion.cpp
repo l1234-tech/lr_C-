0ac59eb9534c4a0e2613bd2982603d5f87045680
Andrey Syrchin <toxiclipsiz@gmail.com>
2025-10-23 23:39:37 +0300

    Update operasion.cpp

--- Project1/operasion.cpp
+++ Project1/operasion.cpp
@@ -2,12 +2,24 @@
 #include <string>
 # include <vector>
 
-namespace {
-    void add_number(std::vector<double>& v, double num) {
-        for (auto& elem : v) elem += num;
+std::pair <double,double> discriminant(int a, int b, int c) {
+    double D_root;
+    double x1;
+    double x2;
+    if (a == 0) {
+        return {0.0,0.0};
     }
-
-    void nul_number(std::vector<double>& v, double num) {
-        for (auto& elem : v) elem *= num;
-    }
+    D_root = sqrt(b * b - 4 * c * a);
+    if (D_root == 0) {
+        x1 = x2 = -b / (2 * a);
+        return {x1, x2 };
+    } else {}
+    if (D_root < 0) {
+        return {0.0,0.0};
+    } else {}
+    if (D_root > 0) {
+        x1 = (-b + D_root) / (2 * a);
+        x2 = (-b - D_root) / (2 * a);
+        return { x1, x2 };
+    } else {}
 }
