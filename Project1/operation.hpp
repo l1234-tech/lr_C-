8245d166e120744f622d329c47a30b1dc0f0dd66
Andrey Syrchin <toxiclipsiz@gmail.com>
2025-10-23 23:40:15 +0300

    Update operation.hpp

--- Project1/operation.hpp
+++ Project1/operation.hpp
@@ -1,9 +1,4 @@
 #ifndef OPERATION_HPP
 #define OPERATION_HPP
-namespace {
-	void add_number(std::vector<double>& v, double num);
-	void nul_number(std::vector<double>& v, double num);
-}
-
-
+std::pair <double, double> discriminant(int a, int b, int c);
 #endif
