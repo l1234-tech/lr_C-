ee3efd343f0440cb3d4b276a38cad59607720710
Andrey Syrchin <toxiclipsiz@gmail.com>
2025-10-27 15:17:36 +0300

    Update main.cpp

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -7,13 +7,13 @@
 #include <vector>
 #include "operation.hpp"
 #include "problems.hpp"
-
+#include "pointers.hpp"
+#include <cstring>
 using namespace std;
 
 void setupRussianSimple() {
     setlocale(LC_ALL, "rus");
 }
-
 int AGE() {
     auto age = 20.0;
     std::cout << "Введите возраст: ";
@@ -23,8 +23,27 @@ int AGE() {
     return ageFromPtr;
 }
 
+void swap(int a[], unsigned size, int shift) {
+    if (size <= 1) {
+        return;
+    }
+    shift = shift % size;
+    for (int i = 0; i < shift; i++) {
+        int first_elem = a[0];
+        for (int* p = a; p < a + size - 1; p++) {
+            *p = *(p + 1);
+        }
+        a[size - 1] = first_elem;
+    }
+}
+
 int main() {
     setlocale(LC_ALL, "rus");
-    auto res = problems::problem1();
-    std::cout << "Cумма цифр трехзначного числа: " << res << std::endl;
+    /*int a[10] = {1,2};
+    int max_res = 0;
+    auto res = max2(a, a + 10, &max_res);
+    std::cout << "Максимум = " << max_res << std::endl;*/
+    char s[] = "Hello!";
+    auto res = str_of_len(s);
+    std::cout << "Длина строки = " << res;
 }
