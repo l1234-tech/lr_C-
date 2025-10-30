4da12c3503003cfa341ef3b07900eca5c467996b
Andrey Syrchin <toxiclipsiz@gmail.com>
2025-10-30 12:46:26 +0300

    Update main.cpp

--- Project2_hw/main.cpp
+++ Project2_hw/main.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
 #include "exercise_5_14.hpp"
 
-void ex_9() {
-	int N;
-	std::cin >> N;
-	int power_of_2 = 1;
-	while (power_of_2 <= N) {
-		std::cout << power_of_2 << " ";
-		power_of_2 *= 2;
-	}
-}
-
 int main() {
-	auto res = ex_13();
-	std::cout << res;
+	ex_9::ex_9();
 }
