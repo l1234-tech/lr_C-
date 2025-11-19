e07a857f43479291093574d10d9892f0c6dc1290
Andrey Syrchin <toxiclipsiz@gmail.com>
2025-11-19 16:33:32 +0300

    Add files via upload

---
+++ Project3_classes/irr_number.hpp
@@ -0,0 +1,61 @@
+#ifndef IRR_NUMBER_HPP
+#define IRR_NUMBER_HPP
+#include <cmath>
+#include <vector>
+
+namespace Labs
+{
+	class ComplexNumber {
+	private:
+		double re_{};
+		double im_{};
+	public:
+		ComplexNumber(double re = 0, double im = 0) : re_(re), im_(im) {}
+
+		double re() const;
+		double im() const;
+		double abs() const;
+		ComplexNumber pow(unsigned pow) const;
+
+		ComplexNumber operator* (const ComplexNumber& other) const;
+
+		ComplexNumber operator+(const ComplexNumber& other) const;
+
+		ComplexNumber operator-(const ComplexNumber& other) const;
+
+		ComplexNumber operator/(const ComplexNumber& other) const;
+	};
+
+	class Polynom : std::vector<ComplexNumber> {
+	public:
+		ComplexNumber at(const ComplexNumber& x) const;
+
+
+	};
+
+	class Vector {
+	private:
+		double x_{};
+		double y_{};
+		double z_{};
+	public:
+		Vector(double x = 0, double y = 0, double z = 0) : x_(x), y_(y), z_(z) {}
+		double x() const;
+		double y() const;
+		double z() const;
+		double length() const;
+
+		Vector operator+(const Vector& other) const;
+		Vector operator-(const Vector& other) const;
+		double operator*(const Vector& other) const;
+		//скалярное произведение
+
+		void print_vector() const;
+
+		void print_lenght() const;
+
+		void print_prod (const Vector& other) const;
+	};
+}
+
+#endif
---
+++ Project3_classes/main.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include "classes.hpp"
+#include "irr_number.hpp"
+#include <cmath>
+
+namespace Labs
+{
+	double ComplexNumber::re() const {
+		return re_;
+	}
+
+	double ComplexNumber::im() const {
+		return im_;
+	}
+
+	double ComplexNumber::abs() const {
+		return std::sqrt(im_ * im_ + re_ * re_);
+	}
+
+	ComplexNumber ComplexNumber::pow(unsigned n) const {
+		if (n == 0)
+			return { 1, 0 };
+		if (n == 1)
+			return { re_, im_ };
+		return *this * pow(n - 1);
+	}
+
+	ComplexNumber ComplexNumber::operator+(const ComplexNumber& other) const {
+		return ComplexNumber(re_ + other.re(), im_ + other.im());
+	}
+
+	ComplexNumber ComplexNumber::operator-(const ComplexNumber& other) const {
+		return ComplexNumber(re_ - other.re(), im_ - other.im());
+	}
+
+	ComplexNumber ComplexNumber::operator*(const ComplexNumber& other) const {
+		return ComplexNumber(
+			re_ * other.re() - other.im() * im_,
+			re_ * other.im() + other.re() * im_
+		);
+	}
+
+	ComplexNumber ComplexNumber::operator/(const ComplexNumber& other) const {
+		return ComplexNumber(
+			(re_ * other.re() + other.im() * im_) / (other.im() * other.im() + other.re() * other.re()),
+			(im_ * other.re() - other.im() * re_) / (other.im() * other.im() + other.re() * other.re())
+		);
+	}
+
+	std::ostream& operator<<(std::ostream& os, const ComplexNumber& cn) {
+		if (cn.im() < 0) {
+			os << cn.re() << " " << cn.im() << "i";
+			return os;
+		}
+		else {
+			os << cn.re() << " + " << cn.im() << "i";
+		}
+		return os;
+	}
+
+	double Vector::x() const {
+		return x_;
+	}
+
+	double Vector::y() const {
+		return y_;
+	}
+
+	double Vector::z() const {
+		return z_;
+	}
+
+	double Vector::length() const {
+		return std::sqrt(x_ * x_ + y_ * y_ + z_ * z_);
+	}
+
+	Vector Vector::operator+(const Vector& other) const {
+		return Vector(
+			x_ + other.x(), y_ + other.y(), z_ + other.z()
+		);
+	}
+
+	Vector Vector::operator-(const Vector& other) const {
+		return Vector(
+			x_ - other.x(), y_ - other.y(), z_ - other.z()
+		);
+	}
+
+	double Vector::operator*(const Vector& other) const {
+		return (
+			x_ * other.x()+ y_ * other.y() + z_ * other.z()
+		);
+	}
+
+	void Vector::print_vector() const {
+		std::cout << "(" << x_ << ", " << y_ << ", " << z_ << ")";
+	}
+
+	void Vector::print_lenght() const {
+		std::cout << Vector::length();
+	}
+	void Vector::print_prod(const Vector& other) const {
+		std::cout << (*this * other);
+	}
+}
+
+int main() {
+	auto r1 = Labs::ComplexNumber();
+	auto r2 = Labs::ComplexNumber(2, 3);
+	auto res = r1 / r2;
+	auto vec1 = Labs::Vector(1, 1, 1);
+	auto vec2 = Labs::Vector(1, 2, 3);
+	auto res_vec = vec1 + vec2;
+	res_vec.print_vector();
+	//std::cout << res_vec;
+}
